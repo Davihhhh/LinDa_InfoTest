c67d3c54a53ef7bca1769767597a830c7329a751
lin.19211 <lin.19211@LABI1-6PC20>
2023-03-27 08:33:51 +0200

    task 1

---
+++ LinDa_InfoTest/LinDa_Candidato.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinDa_InfoTest
+{
+    abstract class LinDa_Candidato
+    {
+        private int LinDa_matricola;
+        private string LinDa_nome;
+
+
+        public int LinDa_Matricola {
+            get { return LinDa_matricola; }
+            set { LinDa_matricola = value; }
+        }
+        public string LinDa_Nome
+        {
+            get { return LinDa_nome; }
+            set { LinDa_nome = value; }
+        }
+
+        public LinDa_Candidato()
+        {
+            LinDa_Matricola = 0;
+            LinDa_Nome = "";           
+        }
+        public LinDa_Candidato(string nome)
+        {
+            LinDa_Matricola = 0;
+            LinDa_Nome = nome;
+        }
+        public LinDa_Candidato(int matricola)
+        {
+            LinDa_Matricola = matricola;
+            LinDa_Nome = "";
+        }
+        public LinDa_Candidato(int matricola, string nome)
+        {
+            LinDa_Matricola = matricola;
+            LinDa_Nome = nome;
+        }
+
+        public virtual bool isIdoneo()
+        {
+            return ;
+        }
+        public virtual int punteggio()
+        {
+            return ;
+        }
+    }
+}
